3d3ef3b497dfb6629e1f911673628756559aa01a
Henrique Suarez Siqueira <122102096+HSuarez89@users.noreply.github.com>
2024-10-30 15:43:02 -0300

    Finalização

--- myNavigation/src/pages/Contacts/index.js
+++ myNavigation/src/pages/Contacts/index.js
@@ -3,9 +3,33 @@ import { View, Text } from "react-native";
 
 export default function Contacts({navigation}){
     return (
-        <View>
-            <Text>Davi</Text>
-            <Text onPress={() => navigation.navigate('Information')}>Information...</Text>
+        <View style={{marginLeft:20}}>
+            <View>
+                <Text>Davi Dias</Text>
+                <Text>Telefone: (31) 99898-1717</Text>
+                <Text onPress={() => navigation.navigate('Information',
+                    {
+                        nome: 'Davi Dias Lourenço',
+                        telefone: '(31) 99898-1717',
+                        endereco: 'Rua Indiana 1330 - Jardim América',
+                        profissao: 'Médico',
+                        email: 'davidiaslourenco@gmail.com'
+                    }
+                )}style={{backgroundColor: "blue", color: "white", margin: "auto", padding: 10, borderRadius: 20, fontWeight: "bold", marginTop: 10}}>Information</Text>
+            </View>
+            <View style={{marginTop:50}}>
+                <Text>Henrique Suarez</Text>
+                <Text>Telefone: (31) 99120-9350</Text>
+                <Text onPress={() => navigation.navigate('Information',
+                    {
+                        nome: 'Henrique Suarez Siqueira',
+                        telefone: '(31) 99120-9350',
+                        endereco: 'Rua Queluzita 614 ap 203 - Fernão Dias',
+                        profissao: 'Desenvolvedor',
+                        email: 'henriquesuarez89@gmail.com'
+                    }
+                )} style={{backgroundColor: "blue", color: "white", margin: "auto", padding: 10, borderRadius: 20, fontWeight: "bold", marginTop: 10}}>Information</Text>
+            </View>
         </View>
     )
 }
--- myNavigation/src/pages/Information/index.js
+++ myNavigation/src/pages/Information/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export default function Information({navigation}){
+export default function Information({ route }){
     return (
-        <View>
+        <View style={{marginLeft: 20, marginTop: 10}}>
             <Text>Information</Text>
-            <Text>Meu amor!</Text>
-            <Text onPress={() => navigation.navigate('Contacts')}>Contacts...</Text>
+            <Text>Nome: {route.params.nome}</Text>
+            <Text>Telefone: {route.params.telefone}</Text>
+            <Text>Endereço: {route.params.endereco}</Text>
+            <Text>Profissão: {route.params.profissao}</Text>
+            <Text>E-mail: {route.params.email}</Text>
         </View>
     )
 }
